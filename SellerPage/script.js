eb8b0cc62f60ae0d04708f4bc85c11fec21083fa
Hieu <nineoxon2005@gmail.com>
2023-11-01 14:36:30 +0700

    fixing some UI

--- SellerPage/script.js
+++ SellerPage/script.js
@@ -1,30 +1,59 @@
-const icon = document.querySelector(".upload-icon"),
-fileInput = document.querySelector(".file-input"),
-uploadBook = document.querySelector(".book-upload"),
-cancel = document.querySelector(".cancel-upload"),
-submit = document.querySelector(".submit-upload"),
-success = document.querySelector(".success"),
-returnbtn = document.querySelector(".return-btn"),
-upLoad = document.querySelector(".upload");
+const imgCount = 0;
+$(this).ready(function () {
+  $.get("modules/header.html", function (data) {
+    $("body").prepend(data);
+    $(".book-upload").click(function() {
+      $(".mainpage-upload-comic-form").css("display", "flex");
+    });
+  });
 
-icon.addEventListener("click", () =>{
-  fileInput.click();
-});
+  $.get("modules/footer.html", function (data) {
+    $("body").append(data);
+  });
 
-uploadBook.addEventListener("click", () =>{
-  upLoad.classList.add('active');
-});
+  $.get("upload/uploadPopUp.html", function (data) {
+    $(".mainpage-upload-comic-form").append(data);
+    $(".cancel-upload").click(function() {
+      $(".mainpage-upload-comic-form").css("display", "none");
+      $(".img-view").css("display", "none");
+    });
+    $(".submit-upload").click(function() {
+      $(".mainpage-upload-comic-form").css("display", "none");
+      $(".mainpage-upload_success_notification").css("display", "flex");
+    });
+    $(".upload-icon").click(function (){
+      $(".file-input").click();
+    })
+    $(".file-input").change(function(e) {
+      for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+  
+        var file = e.originalEvent.srcElement.files[i];
 
-cancel.addEventListener("click", () =>{
-  upLoad.classList.remove('active');
-});
+        var img = document.createElement("img");
+        
+        var reader = new FileReader();
+        reader.onloadend = function() {
+              img.src = reader.result;
+              img.classList.add("img-view")
+              img.alt = "";
+              $(".img-view").click(function (){
+                $(".preview-img").attr("src", this.src);
+                $(".preview-side").css("display", "flex");
+              });
+              $(".preview-side:not(img)").click(function (){
+                $(".preview-side").css("display", "none");
+              });
+        }
+        reader.readAsDataURL(file);
+        $(".file-input").after(img);
+      }
+    });
+  });
 
-
-submit.addEventListener("click", () =>{
-  success.classList.add('active');
-  upLoad.classList.remove('active');
-});
-
-returnbtn.addEventListener("click", () =>{
-  success.classList.remove('active');
+  $.get("upload/successPopUp.html", function (data) {
+    $(".mainpage-upload_success_notification").append(data);
+    $(".return-btn").click(function () {
+      $(".mainpage-upload_success_notification").css("display", "none");
+    });
+  });
 });
