8f7dd08d6abff2fe30e20af53ecf248ef098b304
TongLuong <tong.luong203@hcmut.edu.vn>
2023-11-19 10:10:52 +0700

    add temple asp.net

---
+++ DA_6Ducks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DA_6Ducks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
