7341e8ad8d23187796203214a07b06fddb621c0b
tungdoanduy <sion080803@gmail.com>
2023-11-22 22:01:57 +0700

    songa

--- DA_6Ducks/Controllers/MainPage.cs
+++ DA_6Ducks/Controllers/MainPage.cs
@@ -5,6 +5,7 @@ namespace DA_6Ducks.Controllers
 {
     public class MainPage : Controller
     {
+        //son ga
         public IActionResult Index()
         {
             return View("~/Views/MainPage/index.cshtml");
