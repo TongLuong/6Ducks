115c2bab792ac9700db26edc88d0c28b815dcbdb
TongLuong <tong.luong203@hcmut.edu.vn>
2023-12-01 19:10:08 +0700

    merge main

--- DA_6Ducks/obj/Debug/net6.0/DA_6Ducks.AssemblyInfo.cs
+++ DA_6Ducks/obj/Debug/net6.0/DA_6Ducks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DA_6Ducks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d776eca4e62e2e85bb9fa57202ca769bb472db")]
 [assembly: System.Reflection.AssemblyProductAttribute("DA_6Ducks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DA_6Ducks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
