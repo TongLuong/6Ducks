409537c76a13ccc9957ddd20407fd6f9f0fdf5cc
JenceSon <cumeo2503@gmail.com>
2023-12-02 21:34:57 +0700

    fix syntax

--- Models/Domain/VoucherApply.cs
+++ Models/Domain/VoucherApply.cs
@@ -2,8 +2,8 @@ namespace DA_6Ducks.Models.Domain
 {
     public class VoucherApply {
         private long billID, voucherID;
-        public long BillID {get => billID; set => billID = value};
-        public long VoucherID {get => voucherID; set => voucherID = value};
+        public long BillID { get => billID; set => billID = value; }
+        public long VoucherID { get => voucherID; set => voucherID = value; }
         
         public VoucherApply (long billID, long voucherID)
         {
