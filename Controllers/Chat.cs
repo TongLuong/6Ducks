fec4899dc313b6d09c0535bf342e9e46762fc18c
tungdoanduy <sion080803@gmail.com>
2023-12-02 22:21:14 +0700

    save progress

--- Controllers/Chat.cs
+++ Controllers/Chat.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 namespace DA_6Ducks.Controllers
 {
     public class Chat : Controller
     {
+        
         public IActionResult Index()
         {
             return View("/Views/user/chat/index.cshtml");
@@ -12,6 +15,62 @@ namespace DA_6Ducks.Controllers
         public IActionResult IndexIframe()
         {
             return View("/Views/user/chat/Chat-iframe/index.cshtml");
+        }
+        [HttpPost]
+        public IActionResult Rating1()
+        {
+            
+            return null;
+        }
+        [HttpPost]
+        public IActionResult Rating2()
+        {
+            return null;
+        }
+        [HttpPost]
+        public IActionResult Rating3()
+        {
+            return null;
+        }
+        [HttpPost]
+        public IActionResult Rating4()
+        {
+            return null;
+        }
+        [HttpPost]
+        public IActionResult Rating5()
+        {
+            return null;
+        }
+
+        public void GetRate(int rate)
+        {
+            
+            List<int> star = new List<int>(5);
+            SqlConnection conn = new SqlConnection(StaticVariable.sqlConnectionString);
+
+            SqlCommand cmd = new SqlCommand("select dbo.numberOfSellerRatings(@SellerID);", conn);
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@SellerID";
+            param.SqlDbType = System.Data.SqlDbType.Int;
+            param.Value = rate;
+
+            cmd.Parameters.Add(param);
+
+            conn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    for (int i=0;i<5;i++)
+                        star[i] = dr.GetInt32(i);
+
+                }
+            }
+
+            conn.Close();
         }
     }
 }
