b45b0ebcc5e0cd7a350a894705a263e0abc35a32
Dương Chí Hiếu <nineoxon2005@gmail.com>
2023-12-04 03:15:49 +0700

    re-form of product item

--- wwwroot/context/seller-main.js
+++ wwwroot/context/seller-main.js
@@ -1,3 +1,19 @@
+function discountPrice() {
+  $(".discount .price").each(function() {
+    var price =  parseFloat($(this).text().replace(/[.]/g, ''));
+    var badge = parseFloat($(".discount .product-item").has($(this)).find(".badge").text()) / 100;
+    var discount = ((1 + badge) * price).toLocaleString() + "đ";
+    $(this).html($("<span>").text($(this).text()));
+    $(this).append($("<span>").text(discount));
+  });
+  $(".price span:nth-child(2)").css({
+    "text-decoration": "none",
+    "margin-left": "10px"
+  })
+  $(".price span:first-child").css("text-decoration", "line-through");
+};
+
+
 $(this).ready(function () {
   $.get("/components/header.html", function (data) {
     $("body").prepend(data);
@@ -12,4 +28,6 @@ $(this).ready(function () {
   $.get("/components/footer.html", function (data) {
     $("body").append(data);
   });
+
+  discountPrice();
 });
