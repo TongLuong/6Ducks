18f68c6d1c1c5bf7543e655d2db89c31ca9f2002
TongLuong <tong.luong203@hcmut.edu.vn>
2023-12-14 12:08:31 +0700

    format code

--- wwwroot/context/upload.js
+++ wwwroot/context/upload.js
@@ -1,57 +1,57 @@
 const imgCount = 0;
 $(document).ready(function () {
-  $(".book-upload").click(function () {
-    $(".mainpage-upload-comic-form").css("display", "flex");
-    $("body, html").css("overflow", "hidden");
-  });
+    $(".book-upload").click(function () {
+        $(".mainpage-upload-comic-form").css("display", "flex");
+        $("body, html").css("overflow", "hidden");
+    });
 
-  $.get("components/uploadPopUp.html", function (data) {
-    $("body").append(data);
-    $(".cancel-upload").click(function () {
-      $(".mainpage-upload-comic-form").css("display", "none");
-      $(".img-view").remove();
-      $("#file, #name, #quantity, #price").val("");
-      $("body, html").css("overflow", "scroll");
-    });
-    $(".submit-upload").click(function () {
-      $(".mainpage-upload-comic-form").css("display", "none");
-      $(".mainpage-upload_success_notification").css("display", "flex");
-      $(".img-view").remove();
-      $("#file, #name, #quantity, #price").val("");
-    });
-    $(".upload-icon").click(function () {
-      $(".file-input").click();
-    });
-    $(".file-input").change(function (e) {
-      for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
-        var file = e.originalEvent.srcElement.files[i];
+    $.get("components/uploadPopUp.html", function (data) {
+        $("body").append(data);
+        $(".cancel-upload").click(function () {
+            $(".mainpage-upload-comic-form").css("display", "none");
+            $(".img-view").remove();
+            $("#file, #name, #quantity, #price").val("");
+            $("body, html").css("overflow", "scroll");
+        });
+        $(".submit-upload").click(function () {
+            $(".mainpage-upload-comic-form").css("display", "none");
+            $(".mainpage-upload_success_notification").css("display", "flex");
+            $(".img-view").remove();
+            $("#file, #name, #quantity, #price").val("");
+        });
+        $(".upload-icon").click(function () {
+            $(".file-input").click();
+        });
+        $(".file-input").change(function (e) {
+            for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+                var file = e.originalEvent.srcElement.files[i];
 
-        var img = document.createElement("img");
+                var img = document.createElement("img");
 
-        var reader = new FileReader();
-        reader.onloadend = function () {
-          img.src = reader.result;
-          img.classList.add("img-view");
-          img.alt = "";
-          $(".img-view").click(function () {
-            $(".preview-img").attr("src", this.src);
-            $(".preview-side").css("display", "flex");
-          });
-          $(".preview-side>i").click(function () {
-            $(".preview-side").css("display", "none");
-          });
-        };
-        reader.readAsDataURL(file);
-        $(".file-input").after(img);
-      }
+                var reader = new FileReader();
+                reader.onloadend = function () {
+                    img.src = reader.result;
+                    img.classList.add("img-view");
+                    img.alt = "";
+                    $(".img-view").click(function () {
+                        $(".preview-img").attr("src", this.src);
+                        $(".preview-side").css("display", "flex");
+                    });
+                    $(".preview-side>i").click(function () {
+                        $(".preview-side").css("display", "none");
+                    });
+                };
+                reader.readAsDataURL(file);
+                $(".file-input").after(img);
+            }
+        });
     });
-  });
 
-  $.get("components/successPopUp.html", function (data) {
-    $("body").append(data);
-    $(".return-btn").click(function () {
-      $(".mainpage-upload_success_notification").css("display", "none");
-      $("body, html").css("overflow", "scroll");
+    $.get("components/successPopUp.html", function (data) {
+        $("body").append(data);
+        $(".return-btn").click(function () {
+            $(".mainpage-upload_success_notification").css("display", "none");
+            $("body, html").css("overflow", "scroll");
+        });
     });
-  });
 });
