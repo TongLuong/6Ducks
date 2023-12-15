62da287ab0d794e12302ddda6b6172558c6897d2
Dương Chí Hiếu <nineoxon2005@gmail.com>
2023-12-15 20:25:00 +0700

    fix upload

--- wwwroot/context/seller-info.js
+++ wwwroot/context/seller-info.js
@@ -1,61 +1,50 @@
 $(this).ready(function () {
-  $.get("/components/header.html", function (data) {
-    $("body").prepend(data);
-    $(".logo").click(function () {
-      location.href = "SellerMainPage";
+    $(".product-item").click(function (e) {
+        e.preventDefault();
+        location.href = "Product";
     });
-  });
 
-  $.get("/components/footer.html", function (data) {
-    $("body").append(data);
-  });
+    $(".seller-contact").click(function (e) {
+        e.preventDefault();
+        location.href = "Chat";
+    });
+    //pagination function
+    $(".pagination p:not(.arrow)").click(function () {
+        $(".pagination > .active").toggleClass("active");
+        $(this).toggleClass("active");
 
-  $(".product-item").click(function (e) {
-    e.preventDefault();
-    location.href = "Product";
-  });
+        $(".product > .active").toggleClass("active");
+        var x = Number($(this).text()[1]);
+        var y = ".product .page-" + x;
+        $(y).toggleClass("active");
+    });
+    //pagination forward
+    $(".pagination p.arrow.forward").click(function () {
+        if ($(".pagination > .active").next()[0] !== $(this)[0]) {
+            var x = $(".pagination > .active");
+            $(".pagination > .active").next().toggleClass("active");
+            x.toggleClass("active");
 
-  $(".seller-contact").click(function (e) {
-    e.preventDefault();
-    location.href = "Chat";
-  });
-  //pagination function
-  $(".pagination p:not(.arrow)").click(function () {
-    $(".pagination > .active").toggleClass("active");
-    $(this).toggleClass("active");
+            var y = $(".product > .active");
+            $(".product > .active").next().toggleClass("active");
+            y.toggleClass("active");
+        }
+    });
+    //pagination back
+    $(".pagination p.arrow.back").click(function () {
+        if ($(".pagination > .active").prev()[0] !== $(this)[0]) {
+            var x = $(".pagination .active");
+            $(".pagination .active").prev().toggleClass("active");
+            x.toggleClass("active");
 
-    $(".product > .active").toggleClass("active");
-    var x = Number($(this).text()[1]);
-    var y = ".product .page-" + x;
-    $(y).toggleClass("active");
-  });
-  //pagination forward
-  $(".pagination p.arrow.forward").click(function () {
-    if ($(".pagination > .active").next()[0] !== $(this)[0]) {
-      var x = $(".pagination > .active");
-      $(".pagination > .active").next().toggleClass("active");
-      x.toggleClass("active");
+            var y = $(".product > .active");
+            $(".product > .active").prev().toggleClass("active");
+            y.toggleClass("active");
+        }
+    });
 
-      var y = $(".product > .active");
-      $(".product > .active").next().toggleClass("active");
-      y.toggleClass("active");
-    }
-  });
-  //pagination back
-  $(".pagination p.arrow.back").click(function () {
-    if ($(".pagination > .active").prev()[0] !== $(this)[0]) {
-      var x = $(".pagination .active");
-      $(".pagination .active").prev().toggleClass("active");
-      x.toggleClass("active");
-
-      var y = $(".product > .active");
-      $(".product > .active").prev().toggleClass("active");
-      y.toggleClass("active");
-    }
-  });
-
-  $(".about img").click(function (e) {
-    e.preventDefault();
-    location.href = "SellerInfoBuyer";
-  });
+    $(".about img").click(function (e) {
+        e.preventDefault();
+        location.href = "SellerInfoBuyer";
+    });
 });
--- wwwroot/context/seller-main.js
+++ wwwroot/context/seller-main.js
@@ -13,6 +13,8 @@ function discountPrice() {
   $(".price span:first-child").css("text-decoration", "line-through");
 };
 
+const imgCount = 0;
+
 $(this).ready(function () {
     var urlParams = new URLSearchParams(window.location.search);
     var userID = urlParams.get('user');
@@ -84,17 +86,106 @@ $(this).ready(function () {
             else if (type == 1)
                 location.href = "SellerMainPage" + "?seller=" + userID;
         });
+        var button =
+            `<div class="book-upload">
+              <div>
+                <img src="/assets/images/book.png" alt="" />
+                Đăng bán truyện
+              </div>
+            </div>`;
 
-        if (type == 0)
-            $(".book-upload").css("display", "none");
-        else if (type == 1)
-            $(".book-upload").css("display", "");
+
+        $(".toolbar").append(button);
+        $(".book-upload").click(function () {
+            $(".mainpage-upload-comic-form").css("display", "flex");
+            $("body, html").css("overflow", "hidden");
+        });
 
         $("#search").keyup(filterItem);
     });
 
     $.get("/components/footer.html", function (data) {
         $("body").append(data);
+    });
+
+    $.get("components/uploadPopUp.html", function (data) {
+        $("body").append(data);
+        $(".cancel-upload").click(function () {
+            $(".mainpage-upload-comic-form").css("display", "none");
+            $(".img-view").remove();
+            $("#file, #name, #quantity, #price").val("");
+            $("body, html").css("overflow", "scroll");
+        });
+        $(".submit-upload").click(function () {
+            $(".mainpage-upload-comic-form").css("display", "none");
+            $(".mainpage-upload_success_notification").css("display", "flex");
+            $(".img-view").remove();
+            $("#file, #name, #quantity, #price").val("");
+
+            var urlParams = new URLSearchParams(window.location.search);
+            var seller_id = urlParams.get('seller');
+            var img_path = $("#upload-icon").attr('src');
+            var book_name = $("#name").val();
+            var quantity = $("#quantity").val();
+            var genre = $("#category").text;
+            var price = $("#quantity").val();
+            var category = $("#type").text;
+            //var author = $("#quantity").val();
+            //var publisher = $("#quantity").val();
+
+            $.ajax({
+                url: "SellerMainPage/Upload",
+                data: {
+                    "sellerID": seller_id,
+                    "imgPath": img_path,
+                    "bookName": book_name,
+                    "quantity": quantity,
+                    "genre": genre,
+                    "price": price,
+                    "category": category,
+                    "author": author,
+                    "publisher": publisher
+                },
+                type: "post",
+                success: function (response) {
+                    //display product again
+                }
+            });
+        });
+        $(".upload-icon").click(function () {
+            $(".file-input").click();
+        });
+        $(".file-input").change(function (e) {
+            for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+                var file = e.originalEvent.srcElement.files[i];
+
+                var img = document.createElement("img");
+
+                var reader = new FileReader();
+                reader.onloadend = function () {
+                    img.src = reader.result;
+                    img.classList.add("img-view");
+                    img.alt = "";
+                    $(".img-view").click(function () {
+                        $(".preview-img").attr("src", this.src);
+                        $(".preview-side").css("display", "flex");
+                    });
+                    $(".preview-side>i").click(function () {
+                        $(".preview-side").css("display", "none");
+                    });
+                };
+                reader.readAsDataURL(file);
+                $(".file-input").after(img);
+            }
+        });
+    });
+
+    $.get("components/successPopUp.html", function (data) {
+        $("body").append(data);
+        $(".return-btn").click(function () {
+            $(".mainpage-upload_success_notification").css("display", "none");
+            $("body, html").css("overflow", "scroll");
+        });
     });
 
     discountPrice();
