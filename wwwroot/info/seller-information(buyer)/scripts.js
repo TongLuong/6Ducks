2b3d7b6bb7bac6661467da6c926cb21e6dec4c8f
Dương Chí Hiếu <nineoxon2005@gmail.com>
2023-12-27 02:58:26 +0700

    fix UI

--- wwwroot/info/seller-information(buyer)/scripts.js
+++ wwwroot/info/seller-information(buyer)/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $.get("/components/header.html", function (data) {
         $(".book-upload").css("display", "none");    
         $(".logo").click(function() {
-          location.href = 'MainPage'
+          location.href = '/MainPage'
       });
     });
 });
